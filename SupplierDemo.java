6f4adefe976b2deba21c8236703e396a5366bc23
bharathchary720 <105106308+bharathchary720@users.noreply.github.com>
2022-06-02 11:03:26 +0530

    java program

---
+++ SupplierDemo.java
@@ -0,0 +1,14 @@
+package  JAVA8;
+
+import java.time.LocalDateTime;
+import java.util.function.Supplier;
+
+public class SupplierDemo {
+
+    public static void main(String[] args) {
+        Supplier<LocalDateTime> supplier = () -> LocalDateTime.now();
+        System.out.println(supplier.get());
+
+    }
+
+}
